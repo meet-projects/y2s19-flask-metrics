b60a55f8cbea855a36caedef4df5aa804acd2acc
Caleb <cnoble@mit.edu>
2019-06-23 12:56:35 -0400

    made metrics lab

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# y2s19-flask-metrics
+# Y2 2019 Summer: Flask Metrics
+
+## Setup
+
+Before you start coding, make sure you fork and clone the repository
+for this lab:
+```
+cd ~/Desktop
+git clone https://github.com/YOUR_GITHUB_USERNAME/y2s19-flask-metrics.git
+```
+
+## Metrics Lab
+
+Create two versions of the `logged.html` page named `loggedA.html` and `loggedB.html`. In both of them, put a form with a button, but the button should have different text for each version.
+
+Change the `/logged-in` route in `app.py` so that it will randomly show either `loggedA.html` or `loggedB.html`.
+
+In app.py, make two new routes: "/pressedA" and "/pressedB" and in each of them, use the function `create_metric` to record that a user has pressed a certain button.
+
+In `loggedA.html` and `loggedB.html`, in the `<form>` tag, make the `action` parameter equal "/pressedA" or "/pressedB". This way, when the user presses the button, the POST request will be send to the function you just made that will create the metric.
---
+++ app.py
@@ -0,0 +1,61 @@
+from database import *
+from flask import Flask, request, redirect, render_template
+from flask import session as login_session
+import random
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+
+@app.route('/')
+def home():
+    if 'name' in login_session and login_session['name'] != None:
+        return logged_in()
+    
+    if random.random()<.5:
+        version = "A"
+    else:
+        version = "B"
+    create_metric("visit home",version,datetime.datetime.now(),"Anon")
+    return render_template('home'+version+'.html')
+    
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = get_user(request.form['username'])
+    if user != None and user.verify_password(request.form["password"]):
+        login_session['name'] = user.username
+        return logged_in()
+    else:
+        return home()
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if random.random()<.5:
+        version = "A"
+    else:
+        version = "B"
+    create_metric("sign up",version,datetime.datetime.now(),"")
+    #check that username isn't taken
+    user = get_user(request.form['username'])
+    if user == None:
+        create_user(request.form['username'],request.form['password'])
+    return home()
+
+
+@app.route('/logged-in')
+def logged_in():
+    return render_template('logged.html')
+
+
+@app.route('/logout')
+def logout():
+    login_session['name'] = None
+    return home()
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
